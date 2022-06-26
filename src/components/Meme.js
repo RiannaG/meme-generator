b7536ef3379727451573d7310731cecab8d608c6
RiannaG <giusepperianna.f@gmail.com>
2022-06-26 10:12:00 +0200

    Add: state to meme component\ Add: eventListener to the button to update the state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,25 @@
+import memesdata from "../memesdata";
+import { useState } from "react";
+
 export default function Meme() {
+  const [memeImage, setMemeImage] = useState("");
+
+  function getMemeImage() {
+    const memesArray = memesdata.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    setMemeImage(memesArray[randomNumber].url);
+  }
+
   return (
-    <form>
+    <div className="meme--container">
       <div className="meme--input">
         <input type="text" placeholder="Top Text" />
         <input type="text" placeholder="Bottom Text" />
       </div>
-      <button className="meme--button">Get a new meme image 🖼</button>
-    </form>
+      <button className="meme--button" onClick={getMemeImage}>
+        Get a new meme image 🖼
+      </button>
+      <img src={memeImage} />
+    </div>
   );
 }
